d44a6e75a0ed15cf6cec4400b27fc0a4fb06429b
cristian <cristiancocabjn@gmail.com>
2025-05-15 00:25:09 -0400

    Version 2 de la practica de examen 1

--- practica_exa/controllers/user_index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../models/Connection.php";
-require_once "../models/Usuario.php";
-
-$usuario = new Usuario();
-$lista = $usuario->mostrarLista();
-
-require_once "../views/user_index.php";
-exit;
